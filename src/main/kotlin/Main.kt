2ecd30ad51903f4359d6429bfa6e0b22a2daf07e
Manoel <mnfcosta1@gmail.com>
2023-02-09 19:55:36 -0300

    operador !!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,4 +9,6 @@ fun main(args: Array<String>) {
     println(nome?.length)
 //    operador elvis ?: checa se um valor é nulo, caso for utiliza um valor de segurança
     println(nome?.length ?: "bruh")
+//    operador !! indica que voce tem certeza que o valor mão é nulo, mas caso seja, retorna um NullPointerException
+    println(nome!!.length)
 }
