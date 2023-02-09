8347e60d45df977d45341213c3c9855d84b19b9e
Manoel <mnfcosta1@gmail.com>
2023-02-09 20:19:27 -0300

    ler input em Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,7 @@ import kotlin.io.*
 
 fun main(args: Array<String>) {
 
-    for(i in 10 downTo 0 step(2))
-        println(i)
-
-    var i = 1
-
-    do{
-        println(i)
-        i++
-    }while (i!=11)
+    var i: String = readLine() ?: ""
+    var x: Int = i.toIntOrNull() ?: 0
+    println(x)
 }
