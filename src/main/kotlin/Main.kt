41cb79d5b2626e7d294cb86d1c7666e108ae85ba
Manoel <mnfcosta1@gmail.com>
2023-02-09 19:59:03 -0300

    estrutura básica de repetição

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,12 @@
 import kotlin.io.*
 
 fun main(args: Array<String>) {
+    var x: Int = 1
+    var y: Int = 2
 
-//    Tratamento de null
-//    ? = impede que valores nulos deem bigode
-    var nome: String? = null
+    if(x>y)
+        println("$x é maior que $y")
+    else
+        println("$y é maior que $x")
 
-    println(nome?.length)
-//    operador elvis ?: checa se um valor é nulo, caso for utiliza um valor de segurança
-    println(nome?.length ?: "bruh")
-//    operador !! indica que voce tem certeza que o valor mão é nulo, mas caso seja, retorna um NullPointerException
-    println(nome!!.length)
 }
