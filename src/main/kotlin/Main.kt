6af9498263c006d8f527a1cee239f3c44d9f4ba5
Manoel <mnfcosta1@gmail.com>
2023-02-09 19:52:44 -0300

    tratamento de valores nulos em Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 import kotlin.io.*
 
 fun main(args: Array<String>) {
-    println("Hello, World!")
+
+//    Tratamento de null
+//    ? = impede que valores nulos deem bigode
+    var nome: String? = null
+
+    println(nome?.length)
+//    operador elvis ?: checa se um valor é nulo, caso for utiliza um valor de segurança
+    println(nome?.length ?: "bruh")
 }
