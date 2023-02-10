8481e0acf523620a8f2fafc575ff2e19fa58a8bc
Manoel <mnfcosta1@gmail.com>
2023-02-09 21:08:54 -0300

    exercicios básicos em kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,31 @@ import kotlin.io.*
 
 fun main(args: Array<String>) {
 
-    var i: String = readLine() ?: ""
-    var x: Int = i.toIntOrNull() ?: 0
-    println(x)
+    //Exercicios
+
+//    Ex1
+    /*var x: Int = 10
+    var y: Int = 20
+
+    println("A soma de $x e $y é igual a ${x+y}")*/
+
+//    Ex2
+    /*var nome: String = "Manoel"
+    println("Olá $nome!")*/
+
+//    Ex3
+//    for (i in 1..10)
+//        println(i)
+//    Ex4
+/*    var scan: String = readLine() ?: ""
+    var x: Int = scan.toIntOrNull() ?: 0*/
+
+//    if(x > 0)
+//        println("$x é positivo!")
+//    else if(x == 0)
+//        println("$x é zero!")
+//    else
+//        println("$x é negativo!")
+//    println(if (x>0) "Positivo" else if(x==0)"Zero" else "Negativo")
+
 }
