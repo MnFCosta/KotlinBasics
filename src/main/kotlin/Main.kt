b5960185cd49e4c7653cdfc4dcff8d97fd08718e
Manoel <mnfcosta1@gmail.com>
2023-02-09 21:54:27 -0300

    funções em Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,20 @@
 import kotlin.io.*
 
+//homem das caverna
+fun add(x: Int, y: Int): Int{
+    return x+y
+}
+
+//bruno
+fun addnice(x: Int, y: Int): Int = x+y
+
+//lambda
+val addLambda: (Int,Int) -> Int = {x,y -> x+y}
+
 fun main(args: Array<String>) {
+    println(add(1,2))
+    println(addnice(1,2))
+    println(addLambda(5,7))
 
-    //use mutableListOf para lista mutávies, e ListOf para lista não mutávies
-    val lista = mutableListOf(
-        "Kotlin",
-        "Java",
-        "JS",
-        "Python",
-        "C",
-    )
 
-    //usado para adicionar elementos a lista mutável
-    lista.add("C++")
-
-    //the virgin
-    for (i in 0..lista.size-1)
-        println(lista[i])
-    //the chad
-    for (i in lista)
-        println(i)
-    //the thad
-    lista.forEach{lang->
-        println(lang)
-    }
-    //the lad
-    lista.forEachIndexed {indice, linguagem ->
-        println("$indice -> $linguagem")
-    }
-
-    //criar uma lista filtrada
-    val listaFiltrada = lista.filter {
-        it.contains("C")
-    }
-
-    listaFiltrada.forEach{lang->
-        println(lang)
-    }
 }
